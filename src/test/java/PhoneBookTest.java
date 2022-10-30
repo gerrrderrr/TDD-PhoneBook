f24088dd2f5dfede4cb259b79329bf0dd3c28cac
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2022-10-30 13:16:23 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest() {
+        PhoneBook book = new PhoneBook();
+        final int amountExpected = 1;
+        String name = "TEST NAME";
+        String phoneNumber = "TEST PHONE NUMBER";
+        assert book.add(name, phoneNumber) == amountExpected : " оличество номеров в телефонной книге не соответствует за€вленному";
+    }
 }
