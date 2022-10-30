ebae91307fe94f7c1dd5096f1608ebcff453ed2e
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2022-10-30 13:18:29 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,21 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private final Map<String, String> phoneBook;
+
+    public PhoneBook() {
+        this.phoneBook = new TreeMap<>();
+    }
+
     public int add(String name, String phoneNumber) {
+        if (phoneBook.containsValue(phoneNumber)) {
+            System.out.println("Контакт с таким номером существует: " + phoneBook.get(name));
+        } else {
+            phoneBook.put(name, phoneNumber);
+            System.out.println("Контакт " + name + " : " + phoneBook.get(name) + " добавлен");
+            return phoneBook.size();
+        }
         return 0;
     }
 }
